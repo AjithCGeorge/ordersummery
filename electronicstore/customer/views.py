6ffdc3daf3aeef0361ef3ebd5cf64f2b8d7b5012
AjithCGeorge <77489928+AjithCGeorge@users.noreply.github.com>
2021-10-23 10:51:43 +0530

    Update views.py

--- electronicstore/customer/views.py
+++ electronicstore/customer/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect,reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import TemplateView
-from .forms import RegistrationForm,LoginForm,UpdateForm,ReviewForm,PlaceOrderForm,UserAddressForm
+from .forms import RegistrationForm,LoginForm,UpdateForm,ReviewForm,PlaceOrderForm,AddressForm
+    # ,UserAddressForm
 from .models import Cart,Review,Orders,Address
 from seller.models import Products
 from .decorators import signin_required
 from django.utils.decorators import method_decorator
-
+from django.views.generic import ListView, DetailView, View
 
 class RegistrationView(TemplateView):
     form_class = RegistrationForm
@@ -141,38 +142,46 @@ class WriteReview(TemplateView):
             new_review.save()
             return redirect('viewproduct', product.id)
 
-def place_order(request,*args,**kwargs):
-    id=kwargs.get("id")
-    product=Products.objects.get(id=id)
-    instance={
-        "product":product.product_name,
-
-    }
-    form = PlaceOrderForm(initial=instance)
-
-    context={}
-    context["form"]=form
-
-    if request.method=="POST":
-        cid=kwargs.get("cid")
-        # aid = kwargs.get('aid')
-        cart=Cart.objects.get(id=cid)
-
-
-        form=PlaceOrderForm(request.POST,request.user)
-        if form.is_valid():
-            address= form.cleaned_data.get('address')
-            product=product
-            order=Orders(address=address,product=product,user=request.user,seller=product.user.username)
-            print(product.user.username)
-            order.save()
-            
-            cart.status="orderplaced"
-            cart.save()
-
-            return redirect("customer_home")
-
-    return render(request,"placeorder.html",context)
+# def place_order(request,*args,**kwargs):
+#     id=kwargs.get("id")
+#     product=Products.objects.get(id=id)
+#     address=Address.objects.filter(user=request.user)
+#     print(address)
+#     instance={
+#         "product":product.product_name,
+#         'address':address,
+#
+#     }
+#     form = PlaceOrderForm(initial=instance)
+#
+#     context={}
+#     context["form"]=form
+#
+#
+#     if request.method=="POST":
+#         cid=kwargs.get("cid")
+#         # aid = kwargs.get('aid')
+#         cart=Cart.objects.get(id=cid)
+#         # print('Hello',request.POST.get('radioaddress'))
+#
+#         # form=PlaceOrderForm(request.POST,request.user)
+#
+#             # address= form.cleaned_data.get('address')
+#         address=request.POST.get('radioaddress')
+#         # address=Address.objects.get(user=request.user)
+#         print(address)
+#         product=product
+#         order=Orders(product=product,user=request.user,seller=product.user.username,address=address)
+#         print(product.user.username)
+#         order.save()
+#
+#         cart.status="orderplaced"
+#         cart.save()
+#
+#         return redirect("customer_home")
+#
+#
+#     return render(request,"placeorder.html", {'address':address,'product':product,'count':[i for i in range(5)]})
 
 def view_orders(request,*args,**kwargs):
     orders=Orders.objects.filter(user=request.user)
@@ -227,3 +236,170 @@ def delete_address(request,id):
 
     address = Address.objects.filter(pk=id,user=request.user).delete()
     return redirect('view_address')
+
+
+# ===================================================================================================================
+
+def place_order(request,*args,**kwargs):
+    id=kwargs.get("id")
+    product=Products.objects.get(id=id)
+    instance={
+        "product":product.product_name
+    }
+    form=PlaceOrderForm(initial=instance)
+    context={}
+    context["form"]=form
+
+    if request.method=="POST":
+        cid=kwargs.get("cid")
+        cart=Cart.objects.get(id=cid)
+
+        form=PlaceOrderForm(request.POST)
+        if form.is_valid():
+            # address=form.cleaned_data.get("address")
+            product=product
+            order=Orders(product=product,user=request.user,seller=product.user.username)
+            print(product.user.username)
+            order.save()
+            cart.status="orderplaced"
+            cart.save()
+            return redirect("checkout")
+
+    return render(request,"placeorder.html",context)
+
+
+# class CheckoutView(View):
+#     def get(self, *args, **kwargs):
+#         print(self.kwargs)
+#         id = kwargs.get("cid")
+#         # product = Products.objects.get(id=id)
+#         form = AddressForm()
+#         # order = Orders.objects.filter(user=self.request.user, status="ordered")
+#         # print(len(order))
+#         print('user : ', Cart.objects.get(id=id).user)
+#         print('items :',Cart.objects.filter(user=Cart.objects.get(id=id).user))
+#         items=Cart.objects.filter(user=Cart.objects.get(id=id).user,status="ordernotplaced")
+#         cartitems=[]
+#         for i in range(len(items)):
+#             print('item no. {} : {}'.format(i,items[i].product_id))
+#             cartitems.append(items[i])
+#         print(cartitems)
+#         address=Address.objects.filter(user=self.request.user)
+#         print('addresses :',address)
+#         context = {
+#             'form': form,
+#             # 'order': order
+#             'address':address
+#
+#         }
+#         return render(self.request, 'checkout.html',context)
+#     def post(self, *args, **kwargs):
+#         order = Orders.objects.filter(user=self.request.user, status="ordered")
+#         print(order)
+#         print('len of order :',len(order))
+#
+#         form = AddressForm(self.request.POST or None)
+#         if form.is_valid():
+#             street_address = form.cleaned_data.get('street_address')
+#             apartment_address = form.cleaned_data.get('apartment_address')
+#             country = form.cleaned_data.get('country')
+#             zip = form.cleaned_data.get('zip')
+#             save_info = form.cleaned_data.get('save_info')
+#             use_default = form.cleaned_data.get('use_default')
+#             payment_option = form.cleaned_data.get('payment_option')
+#
+#             address = Address(
+#                 user=self.request.user,
+#                 street_address=street_address,
+#                 apartment_address=apartment_address,
+#                 country=country,
+#                 zip=zip,
+#             )
+#             address.save()
+#             if save_info:
+#                 address.default=True
+#                 address.save()
+#
+#
+#             print('address :',address)
+#             order.save()
+#             print(form.cleaned_data)
+#             return redirect('checkout')
+#         else:
+#             print('form invalid')
+#             return redirect('checkout')
+
+def CheckoutView(request):
+    address = Address.objects.filter(user=request.user)
+    print('data :',address)
+
+    addr=[]
+    for i in address:
+        data={}
+        print(i.name)
+        data['name']=i.name
+        data['mob']=i.mob_no
+        data['address']='{}, {}, {}, {}, India, {} '.format(i.house,i.street,i.town,i.state,i.pin)
+        data['landmark']='{}'.format(i.landmark)
+        data['id']=i.id
+        addr.append(data)
+    print('addresses :', addr)
+    context = {
+                'address':addr
+            }
+    if request.method == "POST":
+        print(request.POST)
+        x=request.POST
+        new_address=Address()
+        new_address.user=request.user
+        new_address.name=x['name']
+        new_address.mob_no=x['mob_no']
+        new_address.house=x['house']
+        new_address.street=x['street_address']
+        new_address.town=x['town']
+        new_address.state=x['state']
+        new_address.pin=x['pin']
+        new_address.landmark=x['landmark']
+        if( Address.objects.filter(house=x['house'],pin=x['pin']).exists()):
+            print('already exists')
+        else:
+            new_address.save()
+            print(new_address.street)
+            return redirect("checkout")
+    return render(request, 'checkout.html', context)
+
+def summery(request,*args,**kwargs):
+    print(kwargs.get('id'))
+    print(request.user)
+    cart_item=Cart.objects.filter(user=request.user,status='ordernotplaced')
+
+    for i in cart_item:
+        print(Products.objects.get(id=i.product_id).id)
+        order=Orders()
+        if(Orders.objects.filter(product=Products.objects.get(id=i.product_id))).exists():
+            print('already exists')
+        else:
+            order.product=Products.objects.get(id=i.product_id)
+            order.user=request.user
+            order.seller=Products.objects.get(id=i.product_id).user
+            address=Address.objects.get(id=kwargs.get('id'))
+            ad='{},{},{}, {}, {}, {}, India, {} '.format(address.name,address.mob_no,address.house,address.street,address.town,address.state,address.pin,address.landmark)
+            order.address=ad
+            order.save()
+    data=[]
+    for i in cart_item:
+        content={}
+        product=Products.objects.get(id=i.product_id)
+        print(Products.objects.get(id=i.product_id).image.url)
+        content['image']=product.image.url
+        content['name']=product.product_name.capitalize()
+        content['color']=product.color
+        content['seller']=product.user
+        content['price']=product.price
+        content['offer']=product.offer
+        data.append(content)
+
+    return render(request,'order_summery.html',{'data':data})
+
+def payment(request):
+    return render(request,'payment.html')
